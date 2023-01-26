0a967577ead60bfc57203a241eee8973e52c2179
meghna murali <meghnamurali@meghnas-MacBook-Air.local>
2023-01-25 22:33:15 -0500

    added upload button

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+# @app.route("/upload", methods="POST")
+# def upload_file():
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
